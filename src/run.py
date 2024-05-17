e5c2304f466f1d612162a0a31f2d7b74bf5e2b8a
tgoppelsroed <tgoppelsroed@ethz.ch>
2024-05-18 00:17:02 +0200

    Now possible to use the environment simple_adversary_v3. Also made some other more minor changes.

--- src/run.py
+++ src/run.py
@@ -19,11 +19,12 @@ from MARL_TRAINER import MARL_TRAINER
 def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
-    parser.add_argument("--scenario", type=str, default="simple_spread_v3", help="name of the scenario script")
+    parser.add_argument("--scenario", type=str, default="simple_adversary_v3", help="name of the scenario script")
     parser.add_argument("--num-episodes", type=int, default=int(5e4), help="number of episodes")
     parser.add_argument("--num-good", type=int, default=3, help="number of agents")
-    parser.add_argument("--num-adv", type=int, default=3, help="number of adversaries. if the environment allows for it")
-    parser.add_argument("--num-good-obs", type=int, default=1,
+    parser.add_argument("--num-adv", type=int, default=3,
+                        help="number of adversaries. if the environment allows for it")
+    parser.add_argument("--num-good-obs", type=int, default=3,
                         help="number of good agents observed by other agents critics")
     parser.add_argument("--num-adv-obs", type=int, default=3,
                         help="number of adversaries observed by other agents critics")
@@ -44,21 +45,21 @@ def parse_args():
                              "25)")
     parser.add_argument("--memory", type=int, default=int(1e5), help="size of replay buffer")
     parser.add_argument("--gradclip", type=float, default=1, help="Parameter norm gradient clipping")
-    parser.add_argument("--wd", type=float, default=0.001, help="Optimizer weight decay")
+    parser.add_argument("--wd", type=float, default=0, help="Optimizer weight decay")
     parser.add_argument("--bootstrap", type=bool, default=True, help="starting training with random sampling")
-    parser.add_argument("--eps", type=float, default=0.05, help="epsilon exploration")
+    parser.add_argument("--eps", type=float, default=0, help="epsilon exploration")
     parser.add_argument("--central-critic", type=bool, default=False, help="each group shares a central critic network")
     # Benchmarking
-    parser.add_argument("--save-dir", type=str, default="./src/net_configs/")
+    parser.add_argument("--save-dir", type=str, default="./net_configs/")
     parser.add_argument("--result-name", type=str, default="rewards.csv",
                         help="directory in which training state and model should be saved")
     parser.add_argument("--save-rate", type=int, default=100,
                         help="save model once every time this many episodes are completed")
-    parser.add_argument("--load-dir", type=str, default="./src/net_configs/",
+    parser.add_argument("--load-dir", type=str, default="./net_configs/",
                         help="directory in which training state and model are loaded")
     # Evaluation
     parser.add_argument("--restore", action="store_true", default=False)
-    parser.add_argument("--display", action="store_true", default=True)
+    parser.add_argument("--display", action="store_true", default=False)
     return parser.parse_args()
 
 
@@ -84,11 +85,11 @@ def get_knn(obs, agent, agent_list, num_good_obs, num_adv_obs):
             adv_dist.append(np.linalg.norm(coord - coord_agent) + 1)
         else:
             good_dist.append(np.linalg.norm(coord - coord_agent) + 1)
-    if not adv_dist:
+    if adv_dist:
         adv_dist = np.stack(adv_dist)
         adv_dist_ind = adv_dist.argsort(axis=0)[:num_adv_obs]
         shape = adv_dist.shape[0]
-    else : 
+    else:
         shape = 0
         adv_dist_ind = []
     good_dist = np.stack(good_dist)
@@ -150,7 +151,7 @@ def train(env, BATCH_SIZE, lr, gamma, n_episodes, good_agent_network, adv_agent_
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     env.reset()
-    # TODO change to accommodate other envs
+
     agent_list = env.possible_agents
     agent_indices = {agent: agent_list.index(agent) for agent in agent_list}
     memory = [ReplayMemory(int(memory)) for _ in agent_list]
@@ -244,16 +245,22 @@ if __name__ == '__main__':
                 "simple_world_comm_v3": simple_world_comm_v3}
     if args.scenario == "simple_tag_v3":
         parallel_env = env_dict[args.scenario].parallel_env(continuous_actions=True,
-                                                        render_mode="human" if args.display else None,
-                                                        num_good=args.num_good, num_adversaries=args.num_adv)
+                                                            render_mode="human" if args.display else None,
+                                                            num_good=args.num_good, num_adversaries=args.num_adv)
     elif args.scenario == "simple_spread_v3":
         parallel_env = env_dict[args.scenario].parallel_env(continuous_actions=True,
-                                                        render_mode="human" if args.display else None,
-                                                        N=args.num_good)
+                                                            render_mode="human" if args.display else None,
+                                                            N=args.num_good)
         args.num_adv = 0
         args.num_adv_obs = 0
-    parallel_env.metadata["render_fps"] = float(30)
+    elif args.scenario == "simple_adversary_v3":
+        parallel_env = env_dict[args.scenario].parallel_env(continuous_actions=True,
+                                                            render_mode="human" if args.display else None,
+                                                            N=args.num_good)
+    else:
+        raise Exception("The environment ", args.scenario, " is not implemented")
 
+    parallel_env.metadata["render_fps"] = float(30)
     num_good = 0
     num_adv = 0
     sum_act_size = 0
@@ -307,7 +314,8 @@ if __name__ == '__main__':
                 update_iter=args.update_rate, save_iter=args.save_rate,
                 output_path=args.save_dir, memory=args.memory, load_path=args.load_dir,
                 adv_model=adv_model, good_model=good_model, result_name=args.result_name,
-                bootstrap_sampling=args.bootstrap, eps=args.eps, comb_crit=args.central_critic, wd=args.wd, grad_clip=args.gradclip,
+                bootstrap_sampling=args.bootstrap, eps=args.eps, comb_crit=args.central_critic, wd=args.wd,
+                grad_clip=args.gradclip,
                 num_good_obs=args.num_good_obs, num_adv_obs=args.num_adv_obs, kNN_enabled=args.kNN_enabled)
     else:
         train(env=parallel_env, BATCH_SIZE=args.batch_size, lr=args.lr, gamma=args.gamma, tau=args.tau,
@@ -317,5 +325,6 @@ if __name__ == '__main__':
               update_iter=args.update_rate, save_iter=args.save_rate,
               output_path=args.save_dir, memory=args.memory, load_path=args.load_dir if args.restore else None,
               adv_model=adv_model, good_model=good_model, result_name=args.result_name,
-              bootstrap_sampling=args.bootstrap, eps=args.eps, comb_crit=args.central_critic, wd=args.wd, grad_clip=args.gradclip,
+              bootstrap_sampling=args.bootstrap, eps=args.eps, comb_crit=args.central_critic, wd=args.wd,
+              grad_clip=args.gradclip,
               num_good_obs=args.num_good_obs, num_adv_obs=args.num_adv_obs, kNN_enabled=args.kNN_enabled)
